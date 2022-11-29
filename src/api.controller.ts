b2d2b4743bc49427caceb604fb0b1398c84ee6ce
Javier Negre <javi.negre@gmail.com>
2022-11-29 12:00:23 +0100

    Partially hide ip

--- src/api.controller.ts
+++ src/api.controller.ts
@@ -26,9 +26,16 @@ axios.interceptors.response.use(responseHelpers.handleResponseData);
 const Api = (request: Request) => {
   const { bicingApiBaseUrl, endpoints, cacheConfig } = config;
 
+  const getPartialIp = (ipString: string) => {
+    const match = ipString.match(/(?<partialIp>\d{0,4}(\.|:)\d{0,4})$/);
+    return `(...)${match?.groups?.partialIp}`;
+  };
+
   const logRequest = (method: ApiEndpointType, missHit: 'HIT' | 'MISS') => {
     console.log(
-      `[${new Date().toUTCString()}] 🚲 Bicing Api - ${method} *${missHit}* - ${request.ip}`
+      `[${new Date().toUTCString()}] 🚲 Bicing Api - ${method} *${missHit}* - ${getPartialIp(
+        request.ip
+      )}`
     );
   };
 
